81ede6ff594d54aa1cda9777c329c9f023c1569a
Bob <bob.hickson1@gmail.com>
2018-10-21 18:33:01 +0100

    First commit of music-service

---
+++ src/main/java/com/heliolicity/app/api/music/MusicAPI.java
@@ -0,0 +1,13 @@
+package com.heliolicity.app.api.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAPI {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicAPI.class);
+    }
+
+}
